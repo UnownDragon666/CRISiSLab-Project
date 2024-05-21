079256c279b98ef31f0f854380d8fca68ce1ae8b
LordBee222 <98373758+LordBee222@users.noreply.github.com>
2024-05-21 15:56:42 +1200

    Tuesday 21st Lunch Update

--- arduino.ino
+++ arduino.ino
@@ -1,61 +1,41 @@
 #include <Wire.h>
 #include "SparkFun_LPS28DFW_Arduino_Library.h"
 
-// Create a new sensor object
 LPS28DFW pressureSensor;
 
-// I2C address selection
-uint8_t i2cAddress = LPS28DFW_I2C_ADDRESS_DEFAULT; // 0x5C
-//uint8_t i2cAddress = LPS28DFW_I2C_ADDRESS_SECONDARY; // 0x5D
+uint8_t i2cAddress = LPS28DFW_I2C_ADDRESS_DEFAULT;
 
-void setup()
-{
-    int airPressure;
-    int waterDensity;
-    const EARTHGRAVITY = 9.81;
-    float waterHeight;
-    float waterPressure;
-    float totalPressure;
+float airPressure = 1006.20 * 100;
+int waterDensity = 1000;
+float EARTHGRAVITY = 9.81;
+float waterHeight;
+float waterPressure;
+float totalPressure;
 
+void setup() {
+  Serial.begin(115200);
+  Serial.println("LPS28DFW Example 1 - Basic Readings!");
 
-    // Start serial
-    Serial.begin(115200);
-    Serial.println("LPS28DFW Example 1 - Basic Readings!");
+  Wire.begin();
 
-    // Initialize the I2C library
-    Wire.begin();
+  while (pressureSensor.begin(i2cAddress) != LPS28DFW_OK) {
+    Serial.println("Error: LPS28DFW not connected, check wiring and I2C address!");
+    delay(1000);
+  }
 
-    // Check if sensor is connected and initialize
-    // Address is optional (defaults to 0x5C)
-    while(pressureSensor.begin(i2cAddress) != LPS28DFW_OK)
-    {
-        // Not connected, inform user
-        Serial.println("Error: LPS28DFW not connected, check wiring and I2C address!");
-
-        // Wait a bit to see if connection is established
-        delay(1000);
-    }
-
-    Serial.println("LPS28DFW connected!");
+  Serial.println("LPS28DFW connected!");
 }
 
-void loop()
-{
-    // Get measurements from the sensor. This must be called before accessing
-    // the pressure data, otherwise it will never update
-    pressureSensor.getSensorData();
+void loop() {
+  pressureSensor.getSensorData();
 
-    totalPressure = pressureSensor.data.pressure.hpa * 100;
-    totalPressure - airPressure = waterPressure;
-    waterHeight = waterPressure / (waterDensity * EARTHGRAVITY);
-
-    // Print temperature and pressure
-    Serial.print("Temperature (C): ");
-    Serial.print(pressureSensor.data.heat.deg_c);
-    Serial.print("\t\t");
-    Serial.print("Pressure (hPa): ");
-    Serial.println(pressureSensor.data.pressure.hpa);
-
-    // Only print every second
-    delay(1000);
+  totalPressure = pressureSensor.data.pressure.hpa * 100;
+  waterPressure = totalPressure - airPressure;
+  waterHeight = waterPressure / (waterDensity * EARTHGRAVITY);
+  Serial.print("\n");
+  Serial.print(pressureSensor.data.pressure.hpa);
+  Serial.print("\n");
+  Serial.print(waterHeight);
+  
+  delay(500);
 }

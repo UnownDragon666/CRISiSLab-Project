f5745682ac5274f42b92fa34f3541e0cfc70efc3
TheGOAT <162951145+TheGOAT4Ever@users.noreply.github.com>
2024-06-06 19:39:59 +1200

    Create CrisisiOSApp.swift

---
+++ CrisisiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrisisiOSApp.swift
+//  CrisisiOS
+//
+//  Created by Tanush Reddy Arra on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CrisisIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

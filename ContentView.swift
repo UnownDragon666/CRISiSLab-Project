6cb5a1416c46b386194ab5f88551d2eb7201061a
TheGOAT <162951145+TheGOAT4Ever@users.noreply.github.com>
2024-06-06 19:39:30 +1200

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  CrisisiOS
+//
+//  Created by Tanush Reddy Arra on 06/06/2024.
+//
+
+import SwiftUI
+import SocketIO
+
+struct ContentView: View {
+    @State private var pressure: String = "Pressure: N/A"
+    @State private var waterHeight: String = "Water Height: N/A"
+    
+    let manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.log(true), .compress])
+    var socket: SocketIOClient
+
+    init() {
+        socket = manager.defaultSocket
+    }
+
+    var body: some View {
+        VStack {
+            Text(pressure)
+                .font(.largeTitle)
+                .padding()
+            Text(waterHeight)
+                .font(.title)
+                .padding()
+            Button(action: {
+                self.connectSocket()
+            }) {
+                Text("Connect")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .onAppear(perform: connectSocket)
+    }
+    
+    func connectSocket() {
+        socket.on(clientEvent: .connect) { data, ack in
+            print("Socket connected")
+        }
+
+        socket.on("data") { (dataArray, ack) in
+            if let data = dataArray[0] as? [String: Any],
+               let pressureValue = data["pressure_hpa"] as? Double,
+               let waterHeightValue = data["water_height"] as? Double {
+                DispatchQueue.main.async {
+                    self.pressure = String(format: "Pressure: %.2f hPa", pressureValue)
+                    self.waterHeight = String(format: "Water Height: %.2f cm", waterHeightValue)
+                }
+            }
+        }
+
+        socket.connect()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
